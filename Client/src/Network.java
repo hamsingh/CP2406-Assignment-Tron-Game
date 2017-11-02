a6d9f67cad254de09b8f6077a6e766557c7e7c54
hamsingh <harmon.singh@my.jcu.edu.au>
2017-11-03 06:25:51 +1000

    updated network to be same as server. used as utility class

--- Client/src/Network.java
+++ Client/src/Network.java
@@ -5,34 +5,74 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class Network {
+    private DatagramSocket direct;
+    private MulticastSocket multicast;
+    private int multicastPort;
+    private InetAddress multicastIP;
 
-    private DatagramSocket sendSocket;
-    private MulticastSocket receiveSocket;
+    private String latestReadIP;
+    private int latestReadPort;
 
-    Network(int port) throws IOException {
-        sendSocket = new DatagramSocket(port);
-        receiveSocket = new MulticastSocket(port);
+    public Network(String multicastIP, int multicastPort) throws IOException {
+        this.multicastPort = multicastPort;
+        this.multicastIP = InetAddress.getByName(multicastIP);
+
+        direct = new DatagramSocket();
+
+        multicast = new MulticastSocket(multicastPort);
+        multicast.joinGroup(this.multicastIP);
     }
 
-    void sendRequest(String name, String message, int port) throws IOException {
-        InetAddress address = InetAddress.getLocalHost();
-        String request = name + " " + message;
-        DatagramPacket packet = new DatagramPacket(request.getBytes(), request.length(), address, port);
-        sendSocket.send(packet);
+    public void close() throws IOException {
+        multicast.leaveGroup(multicastIP);
+        multicast.close();
+        direct.close();
     }
 
-    String receiveCommand(int port) throws IOException {
-        byte[] buffer = new byte[1024];//TODO: check if this will receive from multicast server
+    public void send(String targetIP, int targetPort, String message) throws IOException {
+        InetAddress address = InetAddress.getByName(targetIP);
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, targetPort);
+        direct.send(packet);
+    }
+
+    public String read() throws IOException {
+        byte[] buffer = new byte[1024];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        //MulticastSocket socket = new MulticastSocket(port);
-        receiveSocket.joinGroup(InetAddress.getByName("238.254.254.254"));
-        receiveSocket.receive(packet);
+        direct.receive(packet);
 
-        return new String(buffer).trim(); //TODO: see if i can access this by just saying print receiveCommand(PORT) etc.
+        latestReadIP = packet.getAddress().getHostAddress();
+        latestReadPort = packet.getPort();
+
+        return new String(buffer).trim();
     }
 
-    void close() {
-        sendSocket.close();
-        receiveSocket.close();
+    public String getIP() throws IOException {
+        return InetAddress.getLocalHost().getHostAddress();
+    }
+
+    public int getPort() throws IOException {
+        return direct.getLocalPort();
+    }
+
+    public String getLatestReadIP() {
+        return latestReadIP;
+    }
+
+    public int getLatestReadPort() {
+        return latestReadPort;
+    }
+
+    public void broadcast(String message) throws IOException {
+        byte[] buffer = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, multicastIP, multicastPort);
+        multicast.send(packet);
+    }
+
+    public String listen() throws IOException {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        multicast.receive(packet);
+
+        return new String(buffer).trim();
     }
 }
