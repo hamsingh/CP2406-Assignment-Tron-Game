66e152d7a861299916548d660ceb15e52d2fc0ae
hamsingh <harmon.singh@my.jcu.edu.au>
2017-11-08 01:54:22 +1000

    Updated Server should work lol needs testing

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -2,22 +2,40 @@ import java.util.ArrayList;
 import java.io.IOException;
 import javax.swing.JOptionPane;
 
-public class Server {
+public class Server{
     private boolean running = true;
     public static Network server;
-    NetworkReader read = new NetworkReader(); // Initialise receiver thread class
-    NetworkSender send = new NetworkSender(); // Initialise broadcaster thread class
+//    ServerInfo BroadcastServerInfo;
+//    NetworkReader read = new NetworkReader(); // Initialise receiver thread class
+//    NetworkSender send = new NetworkSender(); // Initialise broadcaster thread class
     //Network network = new Network("238.254.254.254", 45565);
     //NetworkReader reader = new NetworkReader("238.254.254.254", 45565); // Initialise receiver thread class
     //NetworkSender sender = new NetworkSender("238.254.254.254", 45565); // Initialise broadcaster thread class
-    final ArrayList<String> players = new ArrayList<>();
 
-    public void main(String[] args) throws Exception {
+    //public static Server serverConsole;
+
+    public Server () throws IOException {
+        //ServerInfo myServerInfo = new ServerInfo();
+    }
+
+    public static void main(String[] args) throws Exception {
+        //serverConsole = new Server();
         server = new Network("238.254.254.254", 45565);
+        final ArrayList<String> players = new ArrayList<>();
+        final ArrayList<String> playerIPs = new ArrayList<>();
+        final ArrayList<String> playerPorts = new ArrayList<>();
+        (new Thread(new ServerInfo())).start();
+//        Thread t = new Thread(Runnable);
+//        myServerInfo.start();
+        ServerInfo myServerInfo = new ServerInfo();
+        Thread thread1 = new Thread(myServerInfo);
+        thread1.start();
+
         int numberPlayers = Integer.parseInt(JOptionPane.showInputDialog("Input Number of Players"));
         int i;
-        for (i = 0; i < numberPlayers; i++) {
+        for (i = 0; i < numberPlayers; i+=1) {
             String message = server.read();
+            System.out.println(message);
             if (message.startsWith("Add Player")) {
                 String[] attributes = message.split(",");
                 String name = attributes[1];
@@ -31,43 +49,43 @@ public class Server {
                     String player = name + "," + x + "," + y + "," + jetWall;
                     players.add(player);
                     server.send(server.getLatestReadIP(), server.getLatestReadPort(), "JOINED");
+                    playerIPs.add(server.getLatestReadIP());
+                    playerPorts.add(Integer.toString(server.getLatestReadPort()));
                 }
             }
         }
-        if (i == numberPlayers) {
-            server.broadcast("PLAYERS");
-            for (int wait = 0; wait < 100; wait++);
-            server.broadcast(players.toString());
+        boolean playersConnecting = true;
+        while (playersConnecting) {
+            if (i == numberPlayers) {
+                myServerInfo.stop();
+                for (int x = 0; x < numberPlayers; x++) {
+                    server.send(playerIPs.get(x),Integer.parseInt(playerPorts.get(x)),players.toString());
+                    String reply = server.read();
+                    if (reply == "PLAYERS ADDED"){
+                        break;
+                    }
+                    else {
+                        server.send(playerIPs.get(x),Integer.parseInt(playerPorts.get(x)),players.toString());
+                    }
+                }
+            }
+            playersConnecting = false;
+            for (int wait = 0; wait < 1000; wait++);
         }
 
-        server.broadcast("START");
+        for (int x = 0; x < numberPlayers; x++) {
+            server.send(playerIPs.get(x),Integer.parseInt(playerPorts.get(x)),"START");
+            //String reply = server.read();
+        }
+
+        //server.broadcast("START");
         System.out.println("Game Running");
-        read.start();
-        send.start();
-    }
 
-    public class NetworkReader implements Runnable {
-        private boolean running;
-
-        private synchronized void start() {
-            if(running) return;
-
-            Thread broadcastThread = new Thread(this);
-            broadcastThread.start();
-            running = true;
-        }
-
-        void stop() throws Exception {
-            if (!running) return;
-
-            server.close();
-            running = false;
-        }
-
-        public void run() {
-            int index = 0;
-            while(running){
-                try {
+        Thread readClientInfo = new Thread(() -> {
+            try {
+                int index = 0;
+                System.out.println("Reader Started");
+                while (true) {
                     String message = server.read();
                     String[] attributes = message.split(",");
                     String name = attributes[0];
@@ -76,40 +94,63 @@ public class Server {
 
                     else
                         index = players.indexOf(name);
-                    players.set(index,message);
+                        players.set(index,message);
                 }
-                catch(Exception error) {
-
-                }
+            } catch (IOException ioE) {
+                ioE.printStackTrace();
+                System.out.println(ioE.toString());
             }
-        }
+        });
+        readClientInfo.start();
+
+        Thread broadcastPlayerInfo = new Thread(() -> {
+            try {
+                System.out.println("Sender Started");
+                while (true) {
+                    server.broadcast(players.toString());
+                }
+            } catch (IOException ioE) {
+                ioE.printStackTrace();
+                System.out.println(ioE.toString());
+            }
+        });
+        broadcastPlayerInfo.start();
     }
 
-    public class NetworkSender implements Runnable{
+    public static class ServerInfo implements Runnable{
         private boolean running;
+        //Server server;
+        private volatile boolean playersConnecting = true;
+
+
+//        public ServerInfo() throws IOException{
+//
+//           // server.broadcastServer = new Network("238.254.254.254", 45565);
+//
+//        }
 
         private synchronized void start() {
-            if(running) return;
-
+            if(playersConnecting) return;
             Thread broadcastThread = new Thread(this);
             broadcastThread.start();
             running = true;
         }
 
-        void stop() throws Exception {
-            if (!running) return;
-
-            server.close();
-            running = false;
+        public void stop() {
+//            if (!playersConnecting) return;
+            System.out.println("Server Information Broadcast Stopped");
+            //server.server.close();
+            playersConnecting = false;
         }
 
         public void run() {
-            while(running) {
+            while(playersConnecting) {
                 try {
-                    server.broadcast(players.toString());
+                    server.broadcast(server.getIP() + "," + server.getPort());
                 }
-                catch (Exception error){
-                    error.printStackTrace();
+                catch (IOException ioE) {
+                    ioE.printStackTrace();
+                    System.out.println(ioE.toString());
                 }
             }
         }
