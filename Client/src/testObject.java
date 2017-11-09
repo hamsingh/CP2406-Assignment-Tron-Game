6860f90e5e3350907c600d646a2ef7566602446d
hamsingh <harmon.singh@my.jcu.edu.au>
2017-11-10 07:14:57 +1000

    created test object

---
+++ Client/src/testObject.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class testObject {
+
+    public void move(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
